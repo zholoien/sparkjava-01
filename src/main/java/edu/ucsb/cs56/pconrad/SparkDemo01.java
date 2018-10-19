f542019b092847471f70745d9d61ffefd7fb2e65
Zach Holoien <zholoien@umail.ucsb.edu>
2018-10-18 21:55:04 -0700

    lab03 first submission

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -21,12 +21,15 @@ public class SparkDemo01 {
 		System.out.println("");						  
 		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
 		System.out.println("");
-		spark.Spark.get("/hello", (req, res) -> "<b>Hello World!</b>\n");
+		String html = "<h1><a href='/hello'>Hello</a> World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/zholoien/sparkjava-01'>this github repo</a></p>\n";
+		spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
 
 		spark.Spark.get("/nihao", (req, res) -> "<b>Ni Hao</b>\n");
 
 		spark.Spark.get("/hola", (req, res) -> "<b>¡Hola!</b>\n");
-		spark.Spark.get("/", (req, res) -> "<h1>Yo</h1>\n");
+		spark.Spark.get("/", (req, res) -> html);
 
 	}
 	
